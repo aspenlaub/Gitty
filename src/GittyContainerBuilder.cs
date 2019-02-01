f94637d9ff9ad5cdd43f398c7a86262647c91ce1
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-01 18:54:19 +0100

    Support Microsoft.Extensions.DependencyInjection

--- src/GittyContainerBuilder.cs
+++ src/GittyContainerBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Gitty.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Autofac;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Gitty {
     public static class GittyContainerBuilder {
@@ -14,6 +15,19 @@ namespace Aspenlaub.Net.GitHub.CSharp.Gitty {
             var componentProvider = new ComponentProvider();
             builder.RegisterInstance(componentProvider.SecretRepository);
             return builder;
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public static IServiceCollection UseGitty(this IServiceCollection services) {
+            services.AddTransient<ICakeInstaller, CakeInstaller>();
+            services.AddTransient<ICakeRunner, CakeRunner>();
+            services.AddTransient<IGitHubUtilities, GitHubUtilities>();
+            services.AddTransient<IGitUtilities, GitUtilities>();
+            services.AddTransient<IProcessRunner, ProcessRunner>();
+
+            var componentProvider = new ComponentProvider();
+            services.AddSingleton(componentProvider.SecretRepository);
+            return services;
         }
     }
 }
