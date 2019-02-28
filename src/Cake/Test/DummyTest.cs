f204ff483c77b8d8492876ee2bf9ae17129598fe
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-28 20:29:38 +0100

    Gitty.Cake must be a test project

---
+++ src/Cake/Test/DummyTest.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Gitty.Cake.Test {
+    [TestClass]
+    public class DummyTest {
+        [TestMethod]
+        public void DummyTestMethod() {
+        }
+    }
+}
